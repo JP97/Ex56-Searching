4739d216b13cfda223d298811248ecb6f862e790
Julian <juli11c0@edu.eal.dk>
2019-03-04 09:00:00 +0100

    Add project files.

---
+++ Ex56-Searching/Search.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex56_Searching
+{
+    public class Search
+    {
+		public static int FindPositionInList_Linear(int[] data, int value)
+		{
+			for (int i = 0; i < data.Length; i++)
+			{
+				if(value == data[i])
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+}
---
+++ SearchTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SearchTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SearchTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7abda668-6f13-455b-86f1-e05b76a5173f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
