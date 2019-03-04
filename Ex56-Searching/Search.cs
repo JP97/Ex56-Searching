2ff07bc4e63c772b41b3b3c7e1df897575b8839f
Julian <juli11c0@edu.eal.dk>
2019-03-04 10:47:14 +0100

    binary search

--- Ex56-Searching/Search.cs
+++ Ex56-Searching/Search.cs
@@ -26,5 +26,50 @@ namespace Ex56_Searching
 			}
 			return -1;
 		}
+
+
+
+
+		public static int FindPositionInList_Binary(int[] data, int value)
+		{
+			int lowerbound = 0;
+			int upperBound = data.Length;
+			int midPoint = 0;
+			bool ikkefundet = true;
+
+			while (ikkefundet && lowerbound < data.Length)
+			{
+				midPoint = lowerbound + (upperBound - lowerbound) / 2;
+
+				if (upperBound < lowerbound)
+				{
+					return -1;
+				} 
+
+				else if(data[midPoint] < value)
+				{
+					lowerbound = midPoint + 1;
+				}
+				else if(data[midPoint] > value)
+				{
+					upperBound = midPoint - 1;
+				}
+				else if(data[midPoint] == value)
+				{
+					ikkefundet = false;
+					return midPoint;
+				}
+			}
+			return -1;
+
+		}
+
+
+		public static int FindPositionInList_Binary()
+		{
+
+
+			return -1:
+		}
 	}
 }
